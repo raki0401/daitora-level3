c4379d1db8c4801d6aebce8b4b4f9f2adc8d7099
牧大介 <raki0401@outlook.jp>
2020-12-07 20:31:14 +0900

    swiper(スライダー)完成

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$(function () {
+  var pagetop = $('#page-top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1000) {
+      pagetop.fadeIn("800");
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
