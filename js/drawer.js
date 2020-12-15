0bb6707e3f8aa057fc6592426904644bbe51b165
牧大介 <raki0401@outlook.jp>
2020-12-15 21:22:20 +0900

    ドロワーメニュー完成！

--- js/drawer.js
+++ js/drawer.js
@@ -1,7 +1,9 @@
 
+
   $(".js-drawer").on("click", function(e){
     e.preventDefault();
     let targetClass = $(this).attr("data-target");
     $("." + targetClass).toggleClass("is-checked");
     return false;
   });
+
