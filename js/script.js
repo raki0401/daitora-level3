56b2229a478bf6a4c2b03a11e55ed8498a6df7ed
牧大介 <raki0401@outlook.jp>
2020-12-16 07:19:57 +0900

    スムーススクロール着手

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
 
-$(function () {
-  var pagetop = $('#page-top');
-  // ボタン非表示
-  pagetop.hide();
-  // 100px スクロールしたらボタン表示
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1000) {
-      pagetop.fadeIn("800");
-    } else {
-      pagetop.fadeOut();
-    }
-  });
-  pagetop.click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 500);
+// $(function () {
+//   var pagetop = $('#page-top');
+//   // ボタン非表示
+//   pagetop.hide();
+//   // 100px スクロールしたらボタン表示
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 1000) {
+//       pagetop.fadeIn("800");
+//     } else {
+//       pagetop.fadeOut();
+//     }
+//   });
+//   pagetop.click(function () {
+//     $('body, html').animate({ scrollTop: 0 }, 500);
+//     return false;
+//   });
+// });
+$(function(){
+
+  $('a[href^="#"]').click(function(){
+    let speed = 800;
+    let id = $(this).attr("href");
+    let target = $("#" == id ? "html" : id);
+    let position = $(target).offset().top;
+    $("html, body").animate({scrollTop: position},speed);
     return false;
+  });
+})
+
+$(function(){
+
+  let pagetop = $('#page-top');
+  pagetop .hide();
+  $(window).on("scroll", function(){
+    if($(this).scrollTop() > 1000){
+      $('#page-top').fadeIn(900);
+    }else{
+      $('#page-top').fadeOut(100);
+    }
   });
 });
