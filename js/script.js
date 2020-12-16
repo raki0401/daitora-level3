15b729cf4b0586e8bf9a12f858d458cbb6a09e18
牧大介 <raki0401@outlook.jp>
2020-12-17 05:57:51 +0900

    一旦コミット。やばくなったらここまで戻る

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+//今までのやつ
+
 // $(function () {
 //   var pagetop = $('#page-top');
 //   // ボタン非表示
@@ -16,27 +18,71 @@
 //     return false;
 //   });
 // });
-$(function(){
 
-  $('a[href^="#"]').click(function(){
-    let speed = 800;
-    let id = $(this).attr("href");
-    let target = $("#" == id ? "html" : id);
-    let position = $(target).offset().top;
-    $("html, body").animate({scrollTop: position},speed);
+
+
+
+//DAY23で自分でカスタマイズしたやつ
+
+// $(function(){
+
+//   $('a[href^="#"]').click(function(){
+//     let speed = 800;
+//     let id = $(this).attr("href");
+//     let target = $("#" == id ? "html" : id);
+//     let position = $(target).offset().top;
+//     $("html, body").animate({scrollTop: position},speed);
+//     return false;
+//   });
+// });
+
+// $(function(){
+
+//   let pagetop = $('#page-top');
+//   pagetop .hide();
+//   $(window).on("scroll", function(){
+//     if($(this).scrollTop() > 1000){
+//       $('#page-top').fadeIn(900);
+//     }else{
+//       $('#page-top').fadeOut(200);
+//     }
+//   });
+// });
+
+
+
+//DAY23の解答そのまま
+
+jQuery(function() {
+
+  // スムーススクロール
+  jQuery('a[href^="#"]').click(function() {
+    let header = jQuery(".header").innerHeight();
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    let position = jQuery(target).offset().top - header;
+    if ("fixed" !== jQuery(".header").css("position")) {
+      position = jQuery(target).offset().top;
+    }
+    if (0 > position) {
+      position = 0;
+    }
+    jQuery("html, body").animate({
+        scrollTop: position
+      },
+      speed
+    );
     return false;
   });
-})
 
-$(function(){
+  // スクロール判定
+  jQuery(window).on("scroll", function() {
 
-  let pagetop = $('#page-top');
-  pagetop .hide();
-  $(window).on("scroll", function(){
-    if($(this).scrollTop() > 1000){
-      $('#page-top').fadeIn(900);
-    }else{
-      $('#page-top').fadeOut(100);
+    if (100 < jQuery(this).scrollTop()) {
+      jQuery('.totop').addClass('is-show');
+    } else {
+      jQuery('.totop').removeClass('is-show');
     }
   });
 });
